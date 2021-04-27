18d1ed239501c0f4a91ac95c3a407829549cb745
Raj Gogri <Raj.gogri@wwindia.com>
2021-04-27 10:24:25 +0530

    Test app

---
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        DataModel _userData = new DataModel();
+        public DataModel UserData { get { return _userData; } set { _userData = value; } }
+
+        public MainPage()
+        {
+            BindingContext = this;
+            InitializeComponent();
+
+            UserData.data = new ObservableCollection<MainModel>();
+            Task.Run(async () => { await GetData(); }); 
+        }
+
+        public async void submit_clicked (object s, EventArgs eventArgs)
+        {
+            //this.submittedFirstName.Text = this.firstName.Text;
+            //this.submittedLastName.Text = this.lastName.Text;
+            //this.submittedEmail.Text = this.email.Text;
+            //GetData();
+            MainModel mvm = new MainModel();
+
+            mvm.firstName = this.firstName.Text;
+            mvm.lastName = this.lastName.Text;
+            mvm.email = this.email.Text;
+            await PostData(mvm);
+            await GetData();
+        }
+
+        public async Task GetData()
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync("https://cila-test-api.herokuapp.com/v1/users");
+            var response = JsonConvert.DeserializeObject<DataModel>(result);
+            UserData = response;
+
+            Device.BeginInvokeOnMainThread(() => {
+                userListView.ItemsSource = null;
+                userListView.ItemsSource = UserData.data;
+            });
+
+        }
+        public async Task PostData(MainModel pmvm)
+        {
+            var client = new HttpClient();
+            string json = JsonConvert.SerializeObject(pmvm);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://cila-test-api.herokuapp.com/v1/users", data);
+
+            if (response.IsSuccessStatusCode)
+                 await DisplayAlert("TestApp", "Data submitted successfully", "Ok");
+        }
+    }
+}
---
+++ TestApp/TestApp/MainViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace TestApp
+{
+    public class MainModel : INotifyPropertyChanged
+    {
+        public string _id { get; set; }
+
+        public string _email;
+        public string email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                OnPropertyChanged("email");
+            }
+        }
+
+
+        public string _firstName;
+        public string firstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged("firstName");
+            }
+        }
+
+
+        public string _lastName;
+        public string lastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged("lastName");
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class DataModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<MainModel> _data;
+        public ObservableCollection<MainModel> data
+        {
+            get { return _data; }
+            set
+            {
+                _data = value;
+                OnPropertyChanged("data");
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+   
+}
